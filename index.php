ef4053445e2fda6f6234ad3ed6746a96525ea126
Mário Panenko <77666995+majosnv@users.noreply.github.com>
2021-11-17 10:10:18 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <link href="https://fonts.googleapis.com/css?family=Popins" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+    <link rel="icon" href="favicon.png" sizes="16x16" type="images/png">
+    <link rel="stylesheet" href="style.css">
+    <title>mariopanenko.6f.sk</title>
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <h4>Cvi</h4>
+        </div>
+        <ul class="nav-links">
+            <li>
+                <a href="index.php" class="active">Domov</a>
+            </li>
+            <li>
+                <a href="okruhy.php">Okruhy</a>
+            </li>
+            <li>
+                <a href="login.php">Prihlásiť sa</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="text" style="padding-top: 8%;">
+        <h2>Cvičenia z informatiky</h2><br>
+        <p>VÝCHOVNO – VZDELÁVACÍ CIEĽ</p><br>
+        <p> <li style="list-style: none;">
+        <i class="fas fa-angle-right"></i> Rozšíriť a doplniť vedomosti, zručnosti a teoretické poznatky v nadväznosti na obsah povinného predmetu informatika
+            </li>
+            <li style="list-style: none;">
+            <i class="fas fa-angle-right"></i> Aplikovať nadobudnuté poznatky pri riešení zložitejších úloh
+            </li>
+            <li style="list-style: none;">
+            <i class="fas fa-angle-right"></i> Získavať praktické zručnosti v tvorbe projektových úloh
+            </li>
+            <li style="list-style: none;">
+            <i class="fas fa-angle-right"></i> Rozšíriť a doplniť vedomosti, zručnosti a teoretické poznatky z vybraných tematických okruhov informatiky potrebných k maturitnej skúške z informatiky
+            </li>
+        </p><br>
+        <p>
+            <div>
+                <button class="btn btn1", onclick="window.location.href='login.php';">Prihlásiť sa</button>
+            </div>
+        </p>
+    </div>
+
+</body>
+
+<footer class="footer">
+    <p>Mário Panenko 2021</p>
+</footer>
+</html>
