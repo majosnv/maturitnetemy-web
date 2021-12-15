d11d9ef476e4ded25111f0ae6095b1f6bca32982
Mário Panenko <77666995+majosnv@users.noreply.github.com>
2021-12-16 00:39:40 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect('localhost','root','','databaza_temy',3306); //Localhost
+    $con = mysqli_connect('localhost','root','','databaza_temy', 3306); //Localhost
     //$con = mysqli_connect('Host','mariopanenko6fsk','Heslo','Databaza', 3309); //Endora
 
     if(!$con)
