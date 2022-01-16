a90d30adc856a151620ca2ea5b06bc000543246d
Mário Panenko <77666995+majosnv@users.noreply.github.com>
2022-01-17 00:39:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Popins" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
     <link rel="icon" href="favicon.png" sizes="16x16" type="images/png">
