1bb30bd86bdcbb648fb03a5896e9a1dff4bf4e76
Mário Panenko <77666995+majosnv@users.noreply.github.com>
2022-03-10 19:09:53 +0100

    Update validation.php

--- validation.php
+++ validation.php
@@ -5,7 +5,7 @@ require('connection.php');
 if(isset($_POST['user'], $_POST['password'])){
   $name = $_POST['user'];
   $pass = $_POST['password'];
-  echo "prebral som m";
+  echo "prebral som";
 }
 
 else echo 'nedefinované';
